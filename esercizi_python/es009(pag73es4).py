c47c089d8896775bd02c6d643e7a5bc12274e0cb
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-10-02 00:50:04 +0200

    Ho modificato l'esercizio del router di compito, ma manca il default gateway ancora

--- esercizi_python/es009(pag73es4).py
+++ esercizi_python/es009(pag73es4).py
@@ -5,8 +5,17 @@ es. 009
 text: calcolare quanti sono i quadrati perfetti minori di 200
 """
 
+import math
+
+#i quadrati perfetti/numeri quadrati sono tutti i numeri interi la cui radice quadrata e' un numero intero
 def main():
+    cont  = 0
+    for num in 200:
+        ris = math.sqrt(cont)
+        if ris.is_integer():
+            cont += 1
     
+    print(f"I quadrati perfetti minori di 200 sono{cont}")
 
 if __name__=="__main__": 
     main()
