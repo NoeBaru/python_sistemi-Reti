0cfdd266485d247e23538a678afe44474aa5457b
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-10-05 17:59:11 +0200

    modifiche es di compito per martedì

--- esercizi_python/es011.py
+++ esercizi_python/es011.py
@@ -13,14 +13,8 @@ lung  = int(input("Inserisci la lunghezza dei lati: "))
 window = turtle.Screen() #creazione schermo
 arrow = turtle.Turtle() #creazione turtle (cursore)
 
-#arrow.speed(3) #velocita'
-
-#arrow.begin_fill()
 for i in range (0, lati): #len da la lunghezza della lista
     arrow.forward(360/lati)
     arrow.left(lung) # va a sinistra
-    #arrow.color('purple')
-#arrow.end_fill()
-#arrow.penup() #per sollevare la penna
 
 window.mainloop() #per tenere aperta la finestra
--- esercizi_python/es012.py
+++ esercizi_python/es012.py
@@ -1,8 +1,20 @@
 """
 Author: Noemi Baruffolo
 date: 3/10/2023
-es. 010
+es. 012
 text: chiedere in input n e disegnare una stella a n punte 
 """
 
 import turtle
+
+lati = int(input("Inserisci il numero di lati: "))
+lung  = int(input("Inserisci la lunghezza dei lati: "))
+
+window = turtle.Screen() #creazione schermo
+arrow = turtle.Turtle() #creazione turtle (cursore)
+
+for i in range (0, lati): #len da la lunghezza della lista
+    arrow.forward(lung)
+    arrow.right(180 - 180/lati)
+
+window.mainloop() #per tenere aperta la finestra
