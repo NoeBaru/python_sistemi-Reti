d8b43b6991f9da3d7eb1de38f50cca5d57da6d25
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2023-10-09 22:06:14 +0200

    correzione esercizi per il 9/10

--- esercizi_python/es011.py
+++ esercizi_python/es011.py
@@ -2,19 +2,38 @@
 Author: Noemi Baruffolo
 date: 3/10/2023
 es. 010
-text: fare i poligioni, ciascuno con colori diversi tutti nella stessa schermata
+text: fare i poligioni, ciascuno con colori diversi tutti nella stessa schermata usando comandi come turtle.penup
 """
 
 import turtle
 
-lati = int(input("Inserisci il numero di lati: "))
-lung  = int(input("Inserisci la lunghezza dei lati: "))
+numP = int(input("Inserisci il numero di poligoni: "))
+lung = int(input("Inserisci la lunghezza del primo lato: "))
+spazioVuoto = 70  # Spazio tra i poligoni
+poligoniRiga = 3  # Numero di poligoni per ogni riga
 
-window = turtle.Screen() #creazione schermo
-arrow = turtle.Turtle() #creazione turtle (cursore)
+window = turtle.Screen()
+arrow = turtle.Turtle()
+colori = ["red", "green", "blue", "orange", "purple", "yellow"]
 
-for i in range (0, lati): #len da la lunghezza della lista
-    arrow.forward(360/lati)
-    arrow.left(lung) # va a sinistra
+x = -((numP - poligoniRiga) * (lung + (spazioVuoto * 2))) / 2
+y = 0
 
-window.mainloop() #per tenere aperta la finestra
+for i in range(3, numP + 3):
+    colore = colori[i % len(colori)]
+    arrow.penup()
+    arrow.goto(x, y)
+    arrow.pendown()
+    arrow.color(colore)
+    arrow.begin_fill()
+    for _ in range(i):
+        arrow.forward(lung)
+        arrow.left(360 / i)
+    arrow.end_fill()
+    x += lung + spazioVuoto
+
+    if (i - 2) % poligoniRiga == 0:
+        x = -((numP - poligoniRiga) * (lung + spazioVuoto)) / 2
+        y -= 2 * lung
+
+window.mainloop()
--- esercizi_python/es012.py
+++ esercizi_python/es012.py
@@ -2,19 +2,21 @@
 Author: Noemi Baruffolo
 date: 3/10/2023
 es. 012
-text: chiedere in input n e disegnare una stella a n punte 
+text: chiedere in input i numero di lati e disegnare una stella con quel numero di punte 
 """
 
 import turtle
 
-lati = int(input("Inserisci il numero di lati: "))
+lati = int(input("Inserisci il numero di lati dispari: "))
 lung  = int(input("Inserisci la lunghezza dei lati: "))
 
 window = turtle.Screen() #creazione schermo
 arrow = turtle.Turtle() #creazione turtle (cursore)
 
+angolo = 180 - 180/lati
+
 for i in range (0, lati): #len da la lunghezza della lista
     arrow.forward(lung)
-    arrow.right(180 - 180/lati)
+    arrow.right(angolo)
 
 window.mainloop() #per tenere aperta la finestra
