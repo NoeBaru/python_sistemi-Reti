715ea02279320da10d48203eba22c02ba8108cc5
Noemi Baruffolo <noemi.baruffolo@itiscuneo.eu>
2024-02-04 17:43:03 +0100

    I added the homework exercise

--- esercizi_python/es060(routing_dizionarioMatriceDiAdiacenza).py
+++ esercizi_python/es060(routing_dizionarioMatriceDiAdiacenza).py
@@ -1,24 +1,51 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def drawGraph(matriceAdiacenza):
-    G = nx.Graph(matriceAdiacenza)
+def drawGraph(graph):
+    G = nx.Graph(graph)
 
-    pos = nx.spring_layout(G)  #Specifica il layout del disegno del grafo
-
+    pos = nx.spring_layout(G)
+    
     nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue', font_size=8, font_color='black', font_family='Arial')
     plt.show()
+
+def prettyPrint(matrice, separatore=' '):
+    for riga in matrice:
+        rigaStr = [str(x) for x in riga]
+        print(separatore.join(rigaStr))
+
+def createGraphFromMatrix(matriceAdiacenza):
+    graph = {}
+    nNodi = len(matriceAdiacenza)
+
+    for i in range(nNodi):
+        graph[i] = [j for j, valore in enumerate(matriceAdiacenza[i]) if valore == 1]
+
+    return graph
 
 def main():
     """
     Author: Noemi Baruffolo
     date: 30/01/2024
-    es. 059
-    text: creare il dizionario della matrice di adiacenza del seguente dizionario
-          d = {0: [2, 3], 1: [2,4], 2: [0,1, 3], 3:[0, 2, 4],4: [1, 3] }
+    es. 060
+    text: creare il dizionario di una matrice di adiacenza data
     """
-    matrice = {0: [2, 3], 1: [2, 4], 2: [0, 1, 3], 3: [0, 2, 4], 4: [1, 3]}
-    drawGraph(matrice)
-    pass #non fa niente, così non da errori nel codice
+    matriceAdiacenza = [
+        [0, 0, 1, 1, 0],
+        [0, 0, 1, 0, 1],
+        [1, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0]
+    ]
+
+    prettyPrint(matriceAdiacenza)
+
+    d = createGraphFromMatrix(matriceAdiacenza)
+    print("\nGrafo creato:")
+    print(d)
+
+    # Disegna il grafo creato
+    drawGraph(d)
+
 if __name__ == '__main__':
     main()
